00648ac809983dcad9ed02e3e80498a21d1d0f7e
FelipeKatao <cataofelipe@gmail.com>
2025-08-31 19:33:20 -0300

    Trim values data

--- Beta/Prolan.py
+++ Beta/Prolan.py
@@ -29,11 +29,27 @@ class Prolan:
     def AddNewIntent(self,Id,Name,Return):
         self.Intent[Id] = {"Name":Name,"Return":Return}
 
+    def TrimBySentence(self,text:str):
+        Sentence_list = []
+        Sentence = ""
+        for i in text.split(" "):
+            if self.DictionaryBase.get(i):
+                if self.DictionaryBase.get(i)["Id"] == 1 and len(Sentence) >=1 or self.DictionaryBase.get(i)["Id"] == 1 and len(Sentence_list) >=1:
+                    Sentence_list.append(Sentence)
+                    Sentence = ""
+            Sentence+=i+" "
+        Sentence_list.append(Sentence)
+        return Sentence_list
 
 v =  Prolan()
 v.AddNewIntent(132,"Criacao_variavel","Criar uma variavel")
+v.AddNewIntent(125,"Criacao_variavel_valor","Criar uma variavel com valor")
 v.AddNewWord("Criar",1,"","")
 v.AddNewWord("variavel",2,"","")
 v.AddNewWord("Nova",3,"","")
-
-print(v.ReturnData("Eu quero criar uma nova variavel")["Return"])
+v.AddNewWord("valor",5,"","")
+print(v.TrimBySentence("Eu quero criar   uma  nova variavel criar variavel com valor 4 "))
+#print(v.ReturnData("Eu quero criar   uma  nova variavel criar variavel com valor 4 ")["Return"])
+Frase = v.TrimBySentence("Eu quero criar   uma  nova variavel criar variavel com valor 4 ")
+for i in Frase:
+    print(v.ReturnData(i))
