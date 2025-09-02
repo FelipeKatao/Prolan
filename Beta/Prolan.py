5c22bfa1fa0505e0e9debeac375c419a03f29395
FelipeKatao <cataofelipe@gmail.com>
2025-09-01 22:25:05 -0300

    feat: Criaçaõ de uma novo sistema de classificação

--- Beta/Prolan.py
+++ Beta/Prolan.py
@@ -9,24 +9,48 @@ class Prolan:
         self.StopSetences = []
         self.ImportantTerm = 1
         self.ErrorIntent = "Intent error: Not exists"
+        self.GetContext = []
+        self.KeyContext = []
+        self.flag = False
 
 
     def Tokenize(self,Text:str):
         List_Tokens = ""
+        self.flag  = False
         for i in Text.split(" "):
              v = self.DictionaryBase.get(i.lower())
              if v is not None:
                 List_Tokens+= str(v.get("Id"))
                 self.VectorResult.append(str(v.get("Id")))
+                # if self.flag == True:
+                #     self.GetContext.append(i)
+                if str(v.get("Type")) == "2":
+                    self.flag = True
+                    self.GetContext.append([])
+                    self.KeyContext.append(i)
+                else:
+                    self.flag = False
+             else:
+                if self.flag == True:
+                    self.GetContext[len(self.GetContext)-1].append(i)
+                
         if List_Tokens == "" : List_Tokens =0
         return int(List_Tokens)
     
+    def CreateVector2d(self):
+        Dict_base = {}
+        id_base = 0 
+        for i in self.KeyContext:
+            Dict_base[i+str(id_base)] = [self.GetContext[id_base]]            
+            id_base+=1
+        return Dict_base
+
     def ReturnData(self,Text:str):
         
         return self.Intent.get(self.Tokenize(Text),self.ErrorIntent)
     
     def AddNewWord(self,Word,Id,Type,op):
-        self.DictionaryBase[Word.lower()] = {"Id":Id,"Type":Type,"Op":op}
+        self.DictionaryBase[Word.lower()] = {"Id":Id,"Type":Type,"Bias":op}
     
     def AddNewIntent(self,Id,Name,Return):
         self.Intent[Id] = {"Name":Name,"Return":Return}
@@ -48,8 +72,11 @@ class TokenVector:
         self.Object = ObjectProg
         self.Text = Text
         self.Value = None
+        self.bias = []
+        self.FeaturesCalcs = []
         if self.Object:
              self.Value =  [self.CreateVector()]
+             self.CreateProps()
 
     def __repr__(self):
         return str(self.Value)
@@ -64,11 +91,33 @@ class TokenVector:
         for  i in Text_sentence:
             for x in i.split(" "):
                 Value = self.Object.DictionaryBase.get(x,0)
-                if Value != 0 : Tokens.append(Value["Id"]) 
+                if Value != 0 : 
+                    Tokens.append(Value["Id"]) 
+                    self.bias.append(Value["Bias"])
                 else:  
                     if len(x) >=1:
+                        self.bias.append(0)
                         Tokens.append(0)
         return Tokens
+    
+    def CreateProps(self):
+        self.FeaturesCalcs = []
+        value_valid = 0
+        for i in self.ClearZeros():
+            for x in i:
+                value_valid+=1
+        self.FeaturesCalcs.append(value_valid)
+        self.FeaturesCalcs.append((abs(len(self.Value) - value_valid)))
+        self.FeaturesCalcs.append(self.Med())
+        self.FeaturesCalcs.append(self.bias)
+        return self.FeaturesCalcs
+
+    def Med(self):
+        value_ = 0
+        for i in self.Value:
+            for x  in i:
+                value_ +=int(x)
+        return value_/len(self.Value)
 
     def ClearZeros(self):
         return [[x for x in y if x!=0] for y in self.Value]   
@@ -80,12 +129,18 @@ class TokenVector:
 v =  Prolan()
 v.AddNewIntent(132,"Criacao_variavel","Criar uma variavel")
 v.AddNewIntent(125,"Criacao_variavel_valor","Criar uma variavel com valor")
-v.AddNewWord("Criar",1,"","")
-v.AddNewWord("variavel",2,"","")
-v.AddNewWord("Nova",3,"","")
-v.AddNewWord("valor",5,"","")
-Frase = "Eu quero criar   uma  nova variavel criar variavel com valor 4"
+v.AddNewWord("Criar",1,"",1)
+v.AddNewWord("variavel",2,2,0.5)
+v.AddNewWord("Nova",3,"",0.5)
+v.AddNewWord("valor",5,2,0.5)
+Frase = "Eu quero criar   uma  nova variavel chamada abc criar variavel com valor 4"
 
 # criação de um novo tipo chamda Vetor de tokens
 Vetor = TokenVector(v,Frase)
 print(Vetor.ClearZeros())
+print(Vetor.FeaturesCalcs)
+v.Tokenize(Frase)
+
+print(v.GetContext)
+print(v.KeyContext)
+print(v.CreateVector2d())
